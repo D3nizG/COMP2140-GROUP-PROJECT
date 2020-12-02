e091cdf67b2de8307b69d40180ba3faabdff9cc8
Anthony <brown.aj663@gmail.com>
2020-12-01 23:38:49 -0600

    Edited SQL files

--- sql/ItemsTableCreate.sql
+++ sql/ItemsTableCreate.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS Items;
 CREATE TABLE Items (
     id INT AUTO_INCREMENT,
     itemName VARCHAR(64),
-    quantity VARCHAR(64),
+    quantity INT,
     price FLOAT(10,2),
     refName VARCHAR(64),
     totalSold INT,
